f5c0fc0d080b636226be1e81d9d70b1801dfd12c
Rz0R <vpa05@mail.ru>
2021-12-31 13:36:08 +1000

    add Abstract view

---
+++ src/view/abstract-view.js
@@ -0,0 +1,25 @@
+import { createElement } from "../render";
+
+class AbstractView {
+
+  #element = null;
+
+  get element() {
+    if (this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  get template() {
+    throw new Error('Abstract method not implemented: get template');
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+
+}
+
+export default AbstractView;
