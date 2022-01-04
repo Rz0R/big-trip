0aa7ab7c84f8520fc306ff7a52de7b3fa3a0cc37
Rz0R <vpa05@mail.ru>
2022-01-04 14:54:00 +1000

    add check to AbstrtactView class

--- src/view/abstract-view.js
+++ src/view/abstract-view.js
@@ -1,10 +1,15 @@
 import { createElement } from '../render';
 
 class AbstractView {
-
   #element = null;
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error('Can\'t instantiate AbstractView, only concrete one.');
+    }
+  }
 
   get element() {
+
     if (!this.#element) {
       this.#element = createElement(this.template);
     }
