8772ed08e821cea91993a8f2b5e724adfcd2ce72
Rz0R <vpa05@mail.ru>
2022-01-23 21:48:14 +1000

    add point update to server

--- src/api-service.js
+++ src/api-service.js
@@ -27,6 +27,18 @@ class ApiService {
       .then(ApiService.parseResponse);
   }
 
+  updatePoint = async (point) => {
+    const response = await this.#load({
+      url: `points/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(this.#adaptToServer(point)),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+    return parsedResponse;
+  }
+
   #load = async ({
     url,
     method = Method.GET,
@@ -42,6 +54,23 @@ class ApiService {
     } catch (err) {
       ApiService.catchError(err);
     }
+  }
+
+  #adaptToServer = (point) => {
+    const adaptedPoint = {
+      ...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom,
+      'date_to': point.dateTo,
+      'is_favorite': point.isFavorite
+    };
+
+    delete adaptedPoint.bacePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   }
 
   static parseResponse = (response) => response.json();
--- src/model/points-model.js
+++ src/model/points-model.js
@@ -46,20 +46,27 @@ class PointsModel extends AbstractObservable {
     return cloneDeep(this.#offers);
   }
 
-  updatePoint = (updateType, update) => {
+  updatePoint = async (updateType, update) => {
     const index = this.#points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t update unexisting point');
     }
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      update,
-      ...this.#points.slice(index + 1),
-    ];
+    try {
+      const response = await this.#apiService.updatePoint(update);
+      const updatedPoint = this.#adaptToClient(response);
 
-    this._notify(updateType, update);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        updatedPoint,
+        ...this.#points.slice(index + 1),
+      ];
+
+      this._notify(updateType, update);
+    } catch (err) {
+      throw new Error('Can\'t update point');
+    }
   }
 
   addPoint = (updateType, update) => {
